1fc0a3e2741c01458130269ed61ccc66ad5050bc
Andres Lorenzo Carvia <wirfenspam@gmail.com>
2021-04-07 08:40:01 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y openjdk-14-jdk python3-pip
-pip3 install flask flask_cors mcstatus
+pip3 install flask mcstatus
 cp funciones/server.properties .
 cd api_minecraft_server
 python3 app.py
